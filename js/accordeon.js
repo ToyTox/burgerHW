90f978c777f292d87b6bcc23ed2aa5a91ec28a9d
ToyTox <toytox@gmail.com>
2021-06-29 22:49:37 +0300

    Updates

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,18 +1,3 @@
-// $(function () {
-//   const btn = [$('.menu-accordeon__item')];
-//   const activeClass = 'menu-accordeon__item-active';
-
-//   btn.forEach((item) => {
-//     item.click(function (e) {
-//       e.preventDefault();
-
-//       item.removeClass(activeClass);
-
-//       $(this).addClass(activeClass);
-//     });
-//   });
-// });
-
 document.addEventListener('DOMContentLoaded', () => {
   const btn = document.querySelectorAll('.menu-accordeon__item');
   const activeClass = 'menu-accordeon__item-active';
---
+++ js/main.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('section');
+  const display = document.querySelector('.maincontent');
+  // const currentSection = document.querySelector('section.active');
+  // const next = currentSection.nextElementSibling;
+  // const prev = currentSection.previousElementSibling;
+
+  let sectionIndex;
+
+  sections.forEach((section, i) => {
+    if (section.classList.contains('active')) {
+      sectionIndex = i;
+      console.log(sectionIndex);
+    }
+  });
+
+  const performTransitionForward = (index) => {
+    const position = `${(index + 1) * -100}%`;
+
+    sections[sectionIndex].classList.remove('active');
+    sections[sectionIndex + 1].classList.add('active');
+
+    display.style.cssText = `transform: translateY(${position})`;
+
+    sectionIndex++;
+  };
+
+  const performTransitionBackward = (index) => {
+    const position = `${(index - 1) * -100}%`;
+
+    sections[sectionIndex].classList.remove('active');
+    sections[sectionIndex - 1].classList.add('active');
+
+    display.style.cssText = `transform: translateY(${position})`;
+
+    sectionIndex--;
+  };
+
+  document.addEventListener('wheel', (e) => {
+    const delta = e.deltaY;
+
+    if (delta > 0) {
+      // console.log();
+      performTransitionForward(sectionIndex);
+    }
+
+    if (delta < 0) {
+      // console.log();
+      performTransitionBackward(sectionIndex);
+    }
+  });
+});
