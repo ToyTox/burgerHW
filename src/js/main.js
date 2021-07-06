40e9477b1fbb6a89dd6c5d28e65bb04789ca9a88
ToyTox <toytox@gmail.com>
2021-07-06 12:15:13 +0300

    add: add scroll to content in main.js. fix: html and scss.

--- src/js/main.js
+++ src/js/main.js
@@ -1,52 +1,91 @@
 document.addEventListener('DOMContentLoaded', () => {
   const sections = document.querySelectorAll('section');
   const display = document.querySelector('.maincontent');
-  // const currentSection = document.querySelector('section.active');
-  // const next = currentSection.nextElementSibling;
-  // const prev = currentSection.previousElementSibling;
-  
+  const dots = document.querySelectorAll('.fixed-menu__item');
+
+
   let sectionIndex;
+  let dotIndex;
 
   sections.forEach((section, i) => {
     if (section.classList.contains('active')) {
       sectionIndex = i;
-      console.log(sectionIndex);
+      // console.log(sectionIndex);
     }
   });
 
-  const performTransitionForward = index => {
+  dots.forEach((dot, i) => {
+    dot.addEventListener('click', () => {
+      prevIndex = 0;
+      dotIndex = i;
+
+      if (dotIndex > prevIndex) {
+        performTransitionForward(dotIndex);
+        prevIndex = dotIndex;
+        console.log(dotIndex);
+      } else {
+        performTransitionBackward(dotIndex);
+        console.log(dotIndex);
+      }
+    })
+  })
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code == 'ArrowDown') {
+      performTransitionForward(sectionIndex);
+    } else if (e.code == 'ArrowUp') {
+      performTransitionBackward(sectionIndex);
+    }
+  })
+
+  // const sectionTransition = (diretion) => {
+
+  //   if (direction == next) {
+  //     performTransitionForward(sectionIndex);
+  //   } else if (diretion == prev) {
+  //     performTransitionBackward(sectionIndex);
+  //   }
+  // }
+
+  const performTransitionForward = (index) => {
     const position = `${(index + 1) * -100}%`;
-    
+
     sections[sectionIndex].classList.remove('active');
     sections[sectionIndex + 1].classList.add('active');
+
+    dots[sectionIndex].classList.remove('fixed-menu__item--active');
+    dots[sectionIndex + 1].classList.add('fixed-menu__item--active');
 
     display.style.cssText = `transform: translateY(${position})`;
 
     sectionIndex++;
-  }
+  };
 
-  const performTransitionBackward = index => {
+  const performTransitionBackward = (index) => {
     const position = `${(index - 1) * -100}%`;
-    
+
     sections[sectionIndex].classList.remove('active');
     sections[sectionIndex - 1].classList.add('active');
+
+    dots[sectionIndex].classList.remove('fixed-menu__item--active');
+    dots[sectionIndex - 1].classList.add('fixed-menu__item--active');
 
     display.style.cssText = `transform: translateY(${position})`;
 
     sectionIndex--;
-  }
+  };
 
-  document.addEventListener('wheel', e => {
+  document.addEventListener('wheel', (e) => {
     const delta = e.deltaY;
-    
-    if(delta > 0) {
+
+    if (delta > 0) {
       // console.log();
       performTransitionForward(sectionIndex);
     }
 
-    if(delta < 0) {
+    if (delta < 0) {
       // console.log();
-      performTransitionBackward(sectionIndex)
+      performTransitionBackward(sectionIndex);
     }
   });
 });
